d27f7b835d25ec3d0f3dcdddfd741812d91ddde4
Himanshu Singh Bisht <205647899+himashooo@users.noreply.github.com>
2025-07-30 07:40:34 +0530

    Update anagram_checker.py

--- 02-String&Operations/anagram_checker.py
+++ 02-String&Operations/anagram_checker.py
@@ -2,7 +2,7 @@
 
 def anagram(str1,str2):
     if sorted(str1)==sorted(str2):          #sorted in alphabetic order/compare
-        return str1 ,str2,"it is a anagram"
+        print(f"{str1},{str2} it is a anagram")
     else:
-        return "not a anagram"
+        print("not a anagram")
 print(anagram("listen","silent"))
