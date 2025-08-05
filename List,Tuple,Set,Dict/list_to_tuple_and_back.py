de3ffdc44086593a6b61ed252479f69d1fe01c4f
Himanshu Singh Bisht <205647899+himashooo@users.noreply.github.com>
2025-08-05 21:15:32 +0530

    Delete List,Tuple,Set,Dict directory

--- List,Tuple,Set,Dict/list_to_tuple_and_back.py
+++
@@ -1,7 +0,0 @@
-#Convert a list into a tuple and back into a list.
-
-l=[1,2,3,5]
-a=tuple(l)          #converted to tuple
-print(a)
-b=list(l)           #convert back to list
-print(b)
--- List,Tuple,Set,Dict/reverse_list.py
+++
@@ -1,7 +0,0 @@
-#Reverse a list without using the reverse() function.
-
-list=["india","england","usa","japan"]
-rev=[]
-for i in list[-1:-5:-1]:        # iterate from last to first
-    rev.append(i)
-print(rev)
