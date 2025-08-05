c8e0f059b51cf67ec33dd68c0d67f2a4f33d43cb
Himanshu Singh Bisht <205647899+himashooo@users.noreply.github.com>
2025-08-05 21:15:18 +0530

    Delete 03-List/Tuple/Set/Dict directory

--- 03-List/Tuple/Set/Dict/remove_zeros_from_list.py
+++
@@ -1,5 +0,0 @@
-#Write a program to remove all zeros from a list.
-
-l=[1,2,3,0,4,0,6,0,0]
-new_=[i for i in l if i!=0]
-print(new_)
--- 03-List/Tuple/Set/Dict/unique_characters_set.py
+++
@@ -1,8 +0,0 @@
-#Create a set of unique characters from a string.
-
-l="Himanshuu".lower()
-a=set()
-for i in l:
-    if l.count(i)==1:
-        a.add(i)
-print(a)
